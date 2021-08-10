09b29589b6f590f0b835ceafd455affb27f080d9
pravin5551 <pravinvpawar5551@gmail.com>
2021-08-10 12:32:16 +0530

    database part added

---
+++ movie_list/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:movie_list/screens/login_screen.dart';
+import 'package:movie_list/screens/note_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'NoteKeeper',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
